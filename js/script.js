33bb3487c8235e806daf686ae310b3b8ec353d4d
nokia6680 <artur.ballod@gmail.com>
2018-10-24 20:38:28 +0200

    480px - full

--- js/script.js
+++ js/script.js
@@ -58,3 +58,65 @@ $(document).ready(function(){
     ]
   });
 });
+
+
+$(document).ready(function(){
+  $('.source-slider__second').slick({
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: true,
+    prevArrow: '<div class="prev-2"></div>',
+    nextArrow: '<div class="next-2"></div>',
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: true,
+          arrows: true,
+          prevArrow: '<div class="prev-2"></div>',
+          nextArrow: '<div class="next-2"></div>'
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: false,
+          dots: true,
+          arrows: true,
+          prevArrow: '<div class="prev-2"></div>',
+          nextArrow: '<div class="next-2"></div>'
+        }
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+          arrows: true,
+          prevArrow: '<div class="prev-2"></div>',
+          nextArrow: '<div class="next-2"></div>'
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: false,
+          dots: true,
+          arrows: true,
+          prevArrow: '<div class="prev-2"></div>',
+          nextArrow: '<div class="next-2"></div>'
+        }
+      }
+    ]
+  });
+});
